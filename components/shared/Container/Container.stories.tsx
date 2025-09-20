9839f1c41fb32453bef0e5386fe2512a27a7120a
Takashi Yamamoto <taka.layer.021@gmail.com>
2025-09-20 18:20:34 +0900

    fix: Container.stories.tsxの構文エラーを修正

--- components/shared/Container/Container.stories.tsx
+++ components/shared/Container/Container.stories.tsx
@@ -5,8 +5,8 @@ const meta = {
   component: Container,
   tags: ['autodocs'],
   parameters: {
-    layout: 'centered'
-  }
+    layout: 'centered',
+  },
 } satisfies Meta<typeof Container>
 
 export default meta
@@ -14,5 +14,10 @@ type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
   args: {
-  }
+    children: (
+      <div className='bg-gray-200 w-full h-20 flex items-center justify-center'>
+        <span className='text-gray-700'>This is inside the Container</span>
+      </div>
+    ),
+  },
 }
